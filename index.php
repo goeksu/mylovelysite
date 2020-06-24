d8fff736e32507148a0f322bf653c0d8bb269293
expl0p1ty <25721443+expl0p1ty@users.noreply.github.com>
2020-06-24 15:46:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,5 +17,5 @@ $date = (new DateTime())->setTimeZone(new DateTimeZone('Europe/Moscow'))->format
 fwrite($file,$hits[0].') '. $date . '| ' . $ip."\n"); 
 fclose($file);
 echo "<center>hi " . $ip . ". preciated to see you here. me? just a star dust, nothing more.. and sometimes code.";
-echo "<br><br><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/wZuW3YvHHLU?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>";
+echo "<br><br><iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/wZuW3YvHHLU?controls=0' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></center>";
 ?>
