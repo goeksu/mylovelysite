124c268ed90a3cb4d43fc0ed1dac2bb01c16b4df
expl0p1ty <25721443+expl0p1ty@users.noreply.github.com>
2020-06-24 15:29:07 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+$file = fopen("ip.txt","a");
+$count_my_page = "ziyaret.txt";
+$hits = file($count_my_page);
+$hits[0] ++;
+$fp = fopen($count_my_page , "w");
+fputs($fp , "$hits[0]");
+fclose($fp);
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+$date = (new DateTime())->setTimeZone(new DateTimeZone('Europe/Moscow'))->format('Y-m-d H:i:s');
+fwrite($file,$hits[0].') '. $date . '| ' . $ip."\n"); 
+fclose($file);
+echo "hi " + $ip + ". preciated to see you here. me? just a star dust, nothing more.. and sometimes code.
+?>
