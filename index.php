91f12e342b4f2c88e57d4ec4443451253b77d61c
goksu <25721443+goeksu@users.noreply.github.com>
2022-03-07 02:35:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,10 +25,10 @@ if (strstr($ipaddresi, ', ')) {
     $ipaddresi = $ips[0];
 }
 
-
+setcookie("0", "Hey my curios friend. This website is simple enough to not to have vulnerabilities -except 3rd parties that i used-. However, I want you to know that I appreciate your effort.", time() + (86400 * 30*12), "/");
+setcookie("1", "It is said that from the outside I look frighteningly serious!!?? No man, I am friendly. Come and tell me these cookies. Cheers!", time() + (86400 * 30*12), "/");
 
 echo "<script src='https://platform.linkedin.com/badges/js/profile.js' async defer type='text/javascript'></script><br><br><br><center>hi <b>" . $ipaddresi . "</b>. preciated to see you here.";
 echo "<br>me? just star dust, nothing more.. and known as who code sometimes.";
-echo '<!-- Hello my curious friend. I also prepared a note for you here. I admit that I am a little sloppy. I think I have reflected the simplicity of my existence here. Or, I was just lazy.-->';
 echo "<br><br><br><iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/wZuW3YvHHLU?controls=0' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe><br><br><br><div class='badge-base LI-profile-badge' data-locale='tr_TR' data-size='medium' data-theme='light' data-type='VERTICAL' data-vanity='ahmetgoksu' data-version='v1'><a class='badge-base__link LI-simple-link' href='https://tr.linkedin.com/in/ahmetgoksu?trk=profile-badge'></a></div><br><br><br>ahmet@goksu.in</center>";
 ?>
