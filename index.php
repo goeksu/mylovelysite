af2dee6fa7e7262fd9d9bad0d000f0cc4695fef0
goksu <25721443+goeksu@users.noreply.github.com>
2022-03-12 01:03:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,7 @@ if (strstr($ipaddresi, ', ')) {
 setcookie("0", "Hey my curios friend. This website is simple enough to not to have vulnerabilities -except 3rd parties that i used-. However, I want you to know that I appreciate your effort.", time() + (86400 * 30*12), "/");
 setcookie("1", "It is said that from the outside I look frighteningly serious!!?? No man, I am friendly. Come and tell me these cookies. Cheers!", time() + (86400 * 30*12), "/");
 
-echo "<script src='https://platform.linkedin.com/badges/js/profile.js' async defer type='text/javascript'></script><br><br><br><center>hi <b>" . $ipaddresi . "</b>. preciated to see you here.";
-echo "<br>me? just star dust, nothing more.. and known as who code sometimes.";
-echo "<br><br><br><iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/wZuW3YvHHLU?controls=0' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe><br><br><br><div class='badge-base LI-profile-badge' data-size='medium' data-theme='dark' data-type='VERTICAL' data-vanity='ahmetgoksu' data-version='v1'><a class='badge-base__link LI-simple-link' href='https://tr.linkedin.com/in/ahmetgoksu?trk=profile-badge'>Ahmet Göksu</a></div><br><br><br>ahmet@goksu.in<br><a href='https://keybase.io/ahmetgoksu'>PGP</a></center>";
-
+echo "<br><br><br><center>hi <b>" . $ipaddresi . "</b>. preciated to see you here.
+<br>me? just star dust, nothing more.. and known as who code sometimes.
+<br><br><br><iframe width='560' height='315' src='https://www.youtube-nocookie.com/embed/wZuW3YvHHLU?controls=0' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe><br><br><br><a href="https://www.linkedin.com/in/ahmetgoksu">LinkedIn</a><br><br><br>ahmet@goksu.in<br><a href='https://keybase.io/ahmetgoksu'>PGP</a></center>";
 ?>
