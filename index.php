6bd7c9d77820dcc415237821fb8e49c28bfadec7
goksu <25721443+goeksu@users.noreply.github.com>
2022-09-21 13:09:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ try {
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  //die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 } 
 
 $sql = "INSERT INTO visitors (ip, url,time)
