0d3c85ba41463dc13eeb8d4b9567bbd02d6f34d0
goksu <25721443+goeksu@users.noreply.github.com>
2022-12-18 22:18:42 +0300

    Create punnycode.js

---
+++ punnycode.js
@@ -0,0 +1,19 @@
+<head>
+<meta name="description" content="Website of a learner who is into Software Development." />
+<meta property="og:title" content="Goksu's personel website." />
+<meta property="og:url" content="https://ahmet.goksu.in" />
+<meta property="og:description" content="Website of a learner who is into Software Development." />
+
+</head>
+
+<script>
+
+
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+if(isSafari && window.location.href == "https://ahmet.goksu.in/"){
+window.location.href = "https://xn--ahmet-fo14d.goksu.in";
+//xn--ahmet-fo14d.goksu.in
+
+}
+
+</script>
